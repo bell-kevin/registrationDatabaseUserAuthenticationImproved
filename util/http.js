56d774c594f23fa5ad3df8f31d961413be9e84fe
Kevin Bell <bellKevin@pm.me>
2023-04-10 14:57:34 -0600

    Update http.js

--- util/http.js
+++ util/http.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const BACKEND_URL = 'https://react-native-course-bef50-default-rtdb.firebaseio.com/';
 
-export function addUser(userData) {
+export async function addUser(userData) {
   console.log('entered addUser function');
-  const response = axios.post(BACKEND_URL + '/users.json', userData);
+  const response = await axios.post(BACKEND_URL + '/users.json', userData);
   console.log('addUser response: ' + response);
+  console.log('addUser response.data: ' + response.data.name);
   const id = response.data.name;
   console.log('addUser id: ' + id);
   return id;
